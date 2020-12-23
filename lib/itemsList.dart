f126421854a9a7249c1d59de6abd5eec313d16c5
Aiden Harrington <aidenkharrington@gmail.com>
2020-12-23 12:59:53 -0800

    item view skeleton functionality complete, need to work on the item view page

--- lib/itemsList.dart
+++ lib/itemsList.dart
@@ -10,6 +10,17 @@ class ItemsList extends StatefulWidget {
 class _ItemsListState extends State<ItemsList> {
   final _todoList = <Item>[];
   final _itemFont = TextStyle(fontSize: 18.0);
+  void _itemView() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Todo Item'),
+        ),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +32,7 @@ class _ItemsListState extends State<ItemsList> {
       ),
       body: _buildList(),
       floatingActionButton: FloatingActionButton(
-        onPressed: null, //navigate to item view
+        onPressed: _itemView,
         tooltip: "Add new item",
         backgroundColor: Colors.green,
         child: Icon(Icons.add),
@@ -56,3 +67,18 @@ class _ItemsListState extends State<ItemsList> {
     );
   }
 }
+
+//todo:
+//when an item is pressed, open item view
+//header is item name, page is the saved data
+//on finish, update the date, on cancel, save no
+//changes but keep the object
+
+//when add item is pressed, create a new item with
+//default data, add it to the list of items and
+//open it like a regular item. Any changes update
+//the item like they would normally do
+//cancelling the item removes it from the stack
+
+//look at how to pass data to a new view
+//should I use the item.dart object to render the view?
